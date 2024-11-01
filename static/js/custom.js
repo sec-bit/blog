615a9c921661fb4b791f5eb9e1c59dc2dc5af597
p0n1 <zp@zerok.io>
2024-11-01 21:51:32 +0800

    try fix cf Rocket Loader issue for mathjax

--- static/js/custom.js
+++ static/js/custom.js
@@ -2,5 +2,10 @@ MathJax = {
     tex: {
         displayMath: [['\\[', '\\]'], ['$$', '$$']],  // block
         inlineMath: [['\\(', '\\)'], ['$', '$']]                  // inline
+    },
+    startup: {
+        pageReady: () => {
+            return MathJax.startup.defaultPageReady();
+        }
     }
 };
